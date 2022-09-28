24968d8e17da8cac79f0733e1747bfdfe17b579c
GeorgeTsif <gtsiff@gmail.com>
2022-09-28 15:55:30 +0300

    Deploy the app successfully

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyArdzxhy66GAZfx-JibQ-ad_KlDcJ-RsIk",
+    authDomain: "redux-ecbf4.firebaseapp.com",
+    databaseURL: "https://redux-ecbf4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "redux-ecbf4",
+    storageBucket: "redux-ecbf4.appspot.com",
+    messagingSenderId: "567151338548",
+    appId: "1:567151338548:web:b4851b0f6db01672009661"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyArdzxhy66GAZfx-JibQ-ad_KlDcJ-RsIk",
+    authDomain: "redux-ecbf4.firebaseapp.com",
+    databaseURL: "https://redux-ecbf4-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "redux-ecbf4",
+    storageBucket: "redux-ecbf4.appspot.com",
+    messagingSenderId: "567151338548",
+    appId: "1:567151338548:web:b4851b0f6db01672009661"
+  }
 };
 
 /*
